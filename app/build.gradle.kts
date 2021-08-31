e9655ca105057b504e3dc11944da6d03369def68
topjohnwu <topjohnwu@gmail.com>
2021-08-31 02:46:29 -0700

    Remove online section in modules fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,7 +115,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:${vNav}")
 
     implementation("androidx.biometric:biometric:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.browser:browser:1.4.0")
     implementation("androidx.preference:preference:1.1.1")
