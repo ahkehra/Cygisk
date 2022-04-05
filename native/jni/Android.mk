RobThePCGuy/Kyubi

Implement Zygisk companion process

native/jni/Android.mk
@@ -32,6 +32,7 @@ LOCAL_SRC_FILES := \
     zygisk/utils.cpp \
     zygisk/hook.cpp \
     zygisk/memory.cpp \
+    zygisk/companion.cpp \
     zygisk/deny/cli.cpp \
     zygisk/deny/utils.cpp \
     zygisk/deny/revert.cpp

ahkehra/Cygisk

native/jni/Android.mk
@@ -35,7 +35,8 @@ LOCAL_SRC_FILES := \
     zygisk/entry.cpp \
     zygisk/utils.cpp \
     zygisk/hook.cpp \
-    zygisk/memory.cpp
+    zygisk/memory.cpp \
+    zygisk/companion.cpp
 
 LOCAL_LDLIBS := -llog
 